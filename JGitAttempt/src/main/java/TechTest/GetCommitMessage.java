65997a82cf1ecb7b6612a63885e54ca71a868bbc
ljt <ningmengkong@126.com>
2018-03-01 16:06:50 +0800

    add JGitHelper.java

---
+++ JGitAttempt/src/main/java/TechTest/GetCommitMessage.java
@@ -0,0 +1,32 @@
+package TechTest;
+
+import java.io.IOException;
+
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+
+import TechTest.helper.JGitHelper;
+
+/**
+ *  @author   :   Magister
+ *  @fileName :   TechTest.GetCommitMessage.java
+ *  
+ *  2018年3月1日	下午2:43:33  
+*/
+
+public class GetCommitMessage {
+	public static void main(String[] args) throws IOException {
+		try(Repository repo = JGitHelper.openJGitCookbookRepository()){
+			Ref head = repo.findRef("refs/heads/master");
+			System.out.println("Found head: " + head);
+			
+			try(RevWalk walk = new RevWalk(repo)){
+				RevCommit commit = walk.parseCommit(head.getObjectId());
+				System.out.println("\nCommit-Message: " + commit.getFullMessage());
+				walk.dispose();
+			}
+		}
+	}
+}
